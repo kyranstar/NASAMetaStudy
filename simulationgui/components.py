Better error handling, improved covariance estimation, (almost) fixed categorical simulation

simulationgui/components.py
@@ -95,13 +95,16 @@ class DistributionsList(QListWidget):
         if self.data_file is None:
             error('No data file specified')
             return
-        num_items = self.model().rowCount()
-        for widget in [self.itemWidget(self.item(i)) for i in range(num_items)]:
-            if widget.type_combo.currentText() == "Normal":
-                if widget.name_input.text() in self.data_file.columns:
-                    col = self.data_file.loc[:, widget.name_input.text()]
-                    widget.mean_input.setText(str(col.mean()))
-                    widget.variance_input.setText(str(col.var()))
+        try:
+            num_items = self.model().rowCount()
+            for widget in [self.itemWidget(self.item(i)) for i in range(num_items)]:
+                if widget.type_combo.currentText() == "Normal":
+                    if widget.name_input.text() in self.data_file.columns:
+                        col = self.data_file.loc[:, widget.name_input.text()]
+                        widget.mean_input.setText(str(col.mean()))
+                        widget.variance_input.setText(str(col.var()))
+        except Exception as e:
+            error(str(e))
 
     def upload_file(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File', '.')
@@ -146,8 +149,8 @@ class DistributionsList(QListWidget):
                 col_portion[entry] += 1.0/num_entries
             # convert to tuple list
             col_portion_list = []
-            for entry in self.data_file[col]:
-                col_portion_list.append((entry, col_portion[entry]))
+            for entry in col_portion:
+                col_portion_list.append((entry, col_portion.get(entry, 0.0)))
             portions[col] = col_portion_list
         return portions
 
@@ -235,8 +238,6 @@ class CorrelationModel(QAbstractTableModel):
         self.layoutChanged.emit()
 
     def flags(self, index):
-        if index.row() == index.column():
-            return Qt.ItemIsEnabled | Qt.ItemIsSelectable
         return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
 
 
@@ -273,9 +274,10 @@ class CorrelationsTable(QTableView):
         data_file = get_super_parent(self).distributions_list.data_file
         if data_file is None:
             return
-        cov = correlations(data_file).loc[self.model()._df.columns, self.model()._df.columns]
+        cov = correlations(data_file[self.model()._df.columns], get_super_parent(self).distributions_list.categorical_cols).loc[self.model(
+        )._df.columns, self.model()._df.columns]
         # Fill diagonal with 1.0s
-        cov.values[[np.arange(len(cov.columns))]*2] = 1.0
+        #cov.values[[np.arange(len(cov.columns))]*2] = 1.0
 
         self.model().set_dataframe(cov)
 
@@ -283,6 +285,8 @@ class CorrelationsTable(QTableView):
         return self.model()._df
 
     def mirror_diagonal(self, topLeft, topRight):
+        if self.model()._df.size == 0:
+            return
         if topLeft.row() == topRight.row() and topLeft.column() == topRight.column():
             col = topLeft.column()
             row = topLeft.row()
@@ -291,6 +295,8 @@ class CorrelationsTable(QTableView):
             self.model().set_dataframe_no_update(df)
 
     def refresh_heatmap(self):
+        if self.model()._df.size == 0:
+            return
         self.model().heatmap = np.zeros(self.model()._df.shape, dtype=(float, 4))
         normalizer = matplotlib.colors.Normalize(
             vmin=np.nanmin(self.model()._df.values), vmax=np.nanmax(self.model()._df.values))

simulationgui/datasampling.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 import pandas as pd
 from utility import calculate_dummy
+from scipy.stats import norm
+import math
 
 _math_funcs = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'cosh', 'degrees',
                'e', 'exp', 'fabs', 'floor', 'fmod', 'frexp', 'hypot', 'ldexp', 'log',
@@ -59,15 +61,21 @@ def get_distribution_samples(data_model, num_samples, true_model_text):
     samples_df = pd.DataFrame(data=samples, columns=data_model.variables)
 
     for cat_col, portions in data_model.cat_portions.items():
-        for i, p in enumerate(samples_df[cat_col]):
-            counter = 0.0
-            # accumulate portions until we go above our random sample
-            for entry, portion in portions:
-                counter += portion
-                if counter > p:
-                    samples_df.loc[i, cat_col] = entry
+        # Calculate inverse cumulative thresholds
+        thresholds = []
+        cumulative_portion = 0.0
+        mean = data_model.mean[data_model.variables.index(cat_col)]
+        stddev = math.sqrt(data_model.cov.loc[cat_col, cat_col])
+        for val, frac in portions:
+            cumulative_portion += frac
+            threshold = norm.ppf(
+                cumulative_portion, loc=mean, scale=stddev)
+            thresholds.append((val, threshold))
+        for i, norm_value in enumerate(samples_df[cat_col]):
+            for val, threshold in thresholds:
+                if norm_value < threshold:
+                    samples_df.loc[i, cat_col] = val
                     break
-
     samples_df = calculate_dummy(samples_df, data_model.cat_portions.keys(), data_model.dummy_cols)
     # Calculate the error term in the regression
     # if not y_variance:

simulationgui/utility.py
@@ -1,14 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import Lasso
+from sklearn.covariance import OAS
 import scipy.stats as stats
 import pandas as pd
 from PyQt5 import QtWidgets
 
 
-def correlations(df):
-    # TODO fancy correlation estimation method
-    return df.corr()
+def correlations(df, categorical_cols):
+    # Pairwise
+    # return df.cov()
+    # remove categorical variables
+    orig_columns = df.columns
+    df = df.drop(categorical_cols, axis='columns')
+    continuous_columns = df.columns
+    # estimate covariance matrix
+    estimator = OAS()
+    estimator.fit(df.values)
+
+    # cov = pd.DataFrame(np.cov(df.values, rowvar=False),
+    #                   index=continuous_columns, columns=continuous_columns)
+    #print("OLD COV")
+    # print(cov)
+    #print("NEW COV")
+    cov = pd.DataFrame(estimator.covariance_,
+                       index=continuous_columns, columns=continuous_columns)
+
+    # Add back categorical variables
+    for cat_col in categorical_cols:
+        cov.loc[:, cat_col] = 0.3  # TODO np.nan
+        cov.loc[cat_col, :] = 0.3  # TODO np.nan
+
+    cov = cov[orig_columns].reindex(orig_columns)
+    # print(cov)
+    return cov
 
 
 def error(msg):